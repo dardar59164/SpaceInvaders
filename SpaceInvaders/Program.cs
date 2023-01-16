74ad6a238091137c6997ac48508dac05916d8ac3
Thomas_D <t.dare@ludus-academie.com>
2023-01-16 16:11:26 +0100

    Start Code

--- SpaceInvaders/Program.cs
+++ SpaceInvaders/Program.cs
@@ -6,5 +6,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+namespace SpaceInvadersGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
