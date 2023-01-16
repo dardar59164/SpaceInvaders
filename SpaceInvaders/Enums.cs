ee707a2ee4d68c4d9e4585703891284ccbd9acc8
Thomas_D <t.dare@ludus-academie.com>
2023-01-16 17:07:17 +0100

    Ajout Enums

---
+++ SpaceInvaders/Enums.cs
@@ -0,0 +1,32 @@
+﻿namespace SnakeGameConsole
+{
+    public enum Direction
+    {
+        None,
+        Left,
+        Right
+    }
+
+    public static class DirectionExtension
+    {
+        public static Direction ToDirection(this ConsoleKey key)
+        {
+            Direction dir;
+            switch (key)
+            {
+                case ConsoleKey.LeftArrow:
+                case ConsoleKey.Q:
+                    dir = Direction.Left;
+                    break;
+                case ConsoleKey.RightArrow:
+                case ConsoleKey.D:
+                    dir = Direction.Right;
+                    break;
+                default:
+                    dir = Direction.None;
+                    break;
+            }
+            return dir;
+        }
+    }
+}
